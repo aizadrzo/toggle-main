6312392b6af0f390368a172f4d2f7d605e15dfcc
aizadrzo <aizadridzoo@gmail.com>
2023-12-16 16:50:18 +0800

    chore: updated with content-writing - text new variant 'h4' - added hero, about and process - edit badge - edit tailwind config file

--- src/components/Base/Text/Text.classnames.ts
+++ src/components/Base/Text/Text.classnames.ts
@@ -4,16 +4,18 @@ export type VariantType =
   | "h1"
   | "h2"
   | "h3"
+  | "h4"
   | "p"
   | "small-p"
   | "small"
   | "display";
 
 export const Tags: Record<VariantType, keyof JSX.IntrinsicElements> = {
-  display: "h2",
+  display: "h1",
   h1: "h1",
   h2: "h2",
   h3: "h3",
+  h4: "h4",
   p: "p",
   "small-p": "span",
   small: "small",
@@ -21,9 +23,10 @@ export const Tags: Record<VariantType, keyof JSX.IntrinsicElements> = {
 
 export const Sizes: Record<VariantType, string> = {
   display: "text-[42px] sm:text-7xl font-bold tracking-tight",
-  h1: "text-[32px] sm:text-[40px] font-semibold",
+  h1: "text-[32px] sm:text-[40px] font-semibold leading-10",
   h2: "text-[28px] sm:text-[32px] font-semibold",
   h3: "text-2xl font-semibold",
+  h4: "text-xl font-semibold",
   p: "text-base",
   "small-p": "text-sm sm:text-md",
   small: "text-xs",
@@ -32,8 +35,8 @@ export const Sizes: Record<VariantType, string> = {
 export const TextCVA = cva("", {
   variants: {
     colorStyle: {
-      primary: "text-primary",
-      secondary: "text-secondary",
+      primary: "text-neutral-900",
+      secondary: "text-gray-500",
       accent: "text-accent",
       red: "text-error",
       green: "text-success",
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,14 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
 	theme: {
-		container: {
-			center: true,
-			padding: {
-				DEFAULT: "1rem",
-				sm: "2rem",
-				lg: "4rem",
-			},
+		spacing: {
+			sm: '16px',
+			md: '24px',
+			lg: '32px',
+			xl: '40px',
+			'2xl': '88px',
+			'3xl': '104px',
 		},
+		extend: {
+			container: {
+				center: true,
+			}
+		}
 	},
 	plugins: [require("daisyui")],
 	daisyui: { themes: ['lofi'] }
